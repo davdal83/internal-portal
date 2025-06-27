452d65fe326613a461cad0be20c34dcbbcd22611
davdal83 <dale.david83@gmail.com>
2025-06-27 12:01:44 -0400

    Update contact.js

--- js/contact.js
+++ js/contact.js
@@ -4,6 +4,7 @@ const SUPABASE_URL = 'https://oxvnggohpjpuucsfwvmz.supabase.co';
 const SUPABASE_ANON_KEY =
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im94dmduZ29ocGpwdXVjc2Z3dm16Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA5NzkyNDgsImV4cCI6MjA2NjU1NTI0OH0.lTD9lI2wUWSxTVBPY4wcdo81O1S87M-ZNqYasAezKQ8';
 
+// Initialize Supabase client
 const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
 async function loadLeadership() {
