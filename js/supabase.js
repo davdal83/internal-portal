4b863778970463575fb36f1fd812ab1a53519aa2
davdal83 <dale.david83@gmail.com>
2025-06-27 13:45:51 -0400

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,5 +1,7 @@
-// supabase.js
+// js/supabase.js
+
 const SUPABASE_URL = 'https://oxvgngohpjpuucsfwvmz.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im94dmduZ29ocGpwdXVjc2Z3dm16Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA5NzkyNDgsImV4cCI6MjA2NjU1NTI0OH0.lTD9lI2wUWSxTVBPY4wcdo81O1S87M-ZNqYasAezKQ8';
 
-const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+// create supabase client from global supabase object (loaded from CDN)
+const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
