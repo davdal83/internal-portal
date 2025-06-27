67f23fc5d05823d981f78f20b23276269c8ecbd9
davdal83 <dale.david83@gmail.com>
2025-06-27 12:47:25 -0400

    Delete js directory

--- js/supabase.js
+++
@@ -1,9 +0,0 @@
-// js/supabase.js
-
-const SUPABASE_URL = 'https://oxvgngohpjpuucsfwvmz.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im94dmduZ29ocGpwdXVjc2Z3dm16Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA5NzkyNDgsImV4cCI6MjA2NjU1NTI0OH0.lTD9lI2wUWSxTVBPY4wcdo81O1S87M-ZNqYasAezKQ8';
-
-const supabaseClient = supabase.createClient(supabaseUrl, supabaseAnonKey);
-
-// Make available globally
-window.supabaseClient = supabaseClient;
