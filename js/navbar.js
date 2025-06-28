43a6c02b70a00d9f803c17e69d9822ba7af14485
davdal83 <dale.david83@gmail.com>
2025-06-28 16:06:32 -0400

    Update navbar.js

--- js/navbar.js
+++ js/navbar.js
@@ -3,18 +3,30 @@ document.addEventListener('DOMContentLoaded', () => {
     <nav class="navbar">
       <div class="nav-logo">
         <img src="images/favicon.ico" alt="Logo" />
-        <span>Papa John's JAX</span>
+        Papa Johns Jax
       </div>
+
       <input type="checkbox" id="menu-toggle" />
       <label for="menu-toggle" class="menu-icon">&#9776;</label>
+
       <ul class="nav-links">
         <li><a href="index.html">Home</a></li>
-        <li><a href="our-stores.html">Our Stores</a></li>
-        <li><a href="promotions.html">Deals</a></li>
+        <li><a href="stores.html">Our Stores</a></li>
+        <li><a href="promotions.html">Promotions</a></li>
         <li><a href="team.html">The Team</a></li>
-        <li><a href="#">Team Access</a></li>
+        <li><a href="login.html">Team Access</a></li>
       </ul>
     </nav>
+    <div class="nav-spacer"></div>
   `;
+
   document.getElementById('nav-container').innerHTML = navHTML;
+
+  // Dynamically size the spacer to match the navbar
+  const navbar = document.querySelector('.navbar');
+  const spacer = document.querySelector('.nav-spacer');
+
+  if (navbar && spacer) {
+    spacer.style.height = `${navbar.offsetHeight + 10}px`; // add a little buffer
+  }
 });
